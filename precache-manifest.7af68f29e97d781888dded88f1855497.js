91e3497b7bf1d7198d7959c4bfa5ff046d3e0885
Carolyn Redlak <carolyn.lynch@gmail.com>
2021-01-02 22:04:46 -0500

    Updates

---
+++ precache-manifest.7af68f29e97d781888dded88f1855497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0127339c135c933761ee",
+    "url": "/My-Professional-Website/static/css/main.4dec8907.chunk.css"
+  },
+  {
+    "revision": "0127339c135c933761ee",
+    "url": "/My-Professional-Website/static/js/main.0127339c.chunk.js"
+  },
+  {
+    "revision": "42aa480362134e2399af",
+    "url": "/My-Professional-Website/static/js/runtime~main.42aa4803.js"
+  },
+  {
+    "revision": "da3f0d9eaaf30453ac99",
+    "url": "/My-Professional-Website/static/js/2.da3f0d9e.chunk.js"
+  },
+  {
+    "revision": "9b82e18173d80142b073b81ace65efda",
+    "url": "/My-Professional-Website/index.html"
+  }
+];
