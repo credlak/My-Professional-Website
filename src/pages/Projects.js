a8983b8ba8d3a4897b56a020f35d59b7801486e6
Carolyn Redlak <carolyn.lynch@gmail.com>
2021-01-02 21:30:35 -0500

    Added structure and filled pages with inital content

---
+++ src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageTemplate from "./PageTemplate";
+
+function Projects(props) {
+	return (
+		<PageTemplate children={
+			<div>
+				I will be starting on a side project very soon! I will add some screenshots that will link to the repos here.
+			</div>
+		}
+		/>
+	);
+}
+
+export default Projects;
---
+++ src/pages/Resume.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PageTemplate from "./PageTemplate";
+
+function Resume(props) {
+	return (
+        <PageTemplate children={
+			<iframe
+				style={{
+			        height:"100%",
+				    width:"100%",
+					margin: "auto",
+					border: "none",
+				}}
+				src="https://docs.google.com/document/d/e/2PACX-1vReLqHhJY8bkccVvQtkJxfnZiVM8ap87dbCwdIhK6JvgKFiMW19caImaNro_QVx-tF5BhcTykVbvLqJ/pub?embedded=true" title="My Resume">
+			</iframe>}
+        />
+	);
+}
+
+export default Resume;
